b30cc56ae0d58f829c1e3b0d5793443fa7613522
sykegaming <35393677+sykegaming@users.noreply.github.com>
2018-01-13 10:27:10 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
 
+const express = require('express');
+const app = express();
+
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+const port = process.env.PORT || 5000;
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// make express look in the `public` directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+
+// set the home page route
+app.get('/', (request, response) => {
+    // ejs render automatically looks in the views folder
+    response.render('index');
+});
+
+app.listen(port, () => {
+    // will echo 'Our app is running on http://localhost:5000 when run locally'
+    console.log('Our app is running on http://localhost:' + port);
+});
+
+setInterval(() => {
+    http.get('http://your-app-name.herokuapp.com');
+  }, 900000);
+
+
 const https = require("https"),
       fs = require("fs"),
       Discord = require("discord.js"),
       bot = new Discord.Client(),
       args = process.argv.slice(2),
       channelPath = __dirname + "/.channels",
-      twitchClientID = twitch_api_key,
+      twitchClientID = "j0dwdjzqkzrsnepoo0a81f71a14zmb",
       interval = 300 * 1000,
-      token = bot_key,
+      token = "NDAxNjEzNzQ4MTY0NjI0Mzg0.DTs-jw.__r2P8Ka8etoeKSTGL94LtI_sgk",
       apiUrl = "https://api.twitch.tv/kraken",
       timeout = 2*60*1000;
 var servers = [];
